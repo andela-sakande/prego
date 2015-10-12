35c2f5cb794d1521572165ee522b36d575b738f3
andela-sakande <surajudeen.akande@andela.com>
2015-10-12 10:41:27 +0100

    Edit route file for authentication

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,3 +15,28 @@ Route::get('/', [
     'uses' => '\Prego\Http\Controllers\HomeController@index',
     'as'   => 'index'
 ]);
+
+/**
+ *  Authentication routes
+ */
+Route::get('/auth/register', [
+    'uses' => '\Prego\Http\Controllers\AuthController@getRegister',
+    'as'   => 'auth.register',
+    'middleware' => ['guest']
+]);
+
+Route::post('/auth/register', [
+    'uses' => '\Prego\Http\Controllers\AuthController@postRegister',
+    'middleware' => ['guest']
+]);
+
+Route::get('/auth/signin', [
+    'uses' => '\Prego\Http\Controllers\AuthController@getLogin',
+    'as'   => 'auth.login',
+    'middleware' => ['guest']
+]);
+
+Route::post('/auth/signin', [
+    'uses' => '\Prego\Http\Controllers\AuthController@postLogIn',
+    'middleware' => ['guest']
+]);
